83d8b56bd64cccf4eeb03432b046f43b8ea92944
Mohamed Fariz <faris872010@gmail.com>
2023-11-09 18:20:07 +0530

    seperate components added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <h2>Expense Tracker</h2>
+  <Header/>
+  <Balance/>
   <div class="container">
-
+      <IncomeExpense/>
+      <TransactionHistory/>
+      <AddTransaction/>
   </div>
 </template>
 <script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpense from './components/IncomeExpense.vue';
+  import TransactionHistory from './components/TransactionHistory.vue';
+  import AddTransaction from './components/AddTransaction.vue';
 </script>
